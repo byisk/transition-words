95e351d7629760d74ec7aa49c882b4d2fed89b5d
Szymon Barczak <byisk.pl@gmail.com>
2018-05-13 23:02:29 +0200

    Change image size on mobile

---
+++ js/change-image-size.js
@@ -0,0 +1,7 @@
+if (window.innerWidth < 361) {
+    document.getElementById("yoast").src="img/yoast-seo-265px.png";
+} else if (window.innerWidth < 467) {
+    document.getElementById("yoast").src="img/yoast-seo-371px.png";
+} else if (window.innerWidth < 562) {
+    document.getElementById("yoast").src="img/yoast-seo-466px.png"
+}
